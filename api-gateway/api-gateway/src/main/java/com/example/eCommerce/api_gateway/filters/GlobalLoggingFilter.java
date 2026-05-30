5da5b13ce88226cd3cf2ef9a11511868a54817a7
Hemant <hem789ant1997@outlook.com>
2026-05-30 23:42:43 +0530

    filter addition in api gateway started

---
+++ api-gateway/api-gateway/src/main/java/com/example/eCommerce/api_gateway/filters/GlobalLoggingFilter.java
@@ -0,0 +1,18 @@
+package com.example.eCommerce.api_gateway.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalLoggingFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("logging from global: {}",exchange.getRequest().getURI());
+        return chain.filter(exchange);
+    }
+}
